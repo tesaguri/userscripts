dc012264308db3d86960d9f59b3ccbbe6691328d
Daiki Mizukami <tesaguriguma@gmail.com>
2024-11-16 21:34:45 +0900

    feat: add `fedibird-hook-bsky-app-links`

---
+++ fedibird-hook-bsky-app-links/fedibird-hook-bsky-app-links.user.js
@@ -0,0 +1,242 @@
+// ==UserScript==
+// @name        fedibird.com - Hook bsky.app links
+// @namespace   https://github.com/tesaguri
+// @match       https://fedibird.com/web/*
+// @grant       none
+// @version     1.0.0
+// @updateURL   https://github.com/tesaguri/userscripts/raw/main/fedibird-hook-bsky-app-links/fedibird-hook-bsky-app-links.user.js
+// @author      Daiki "tesaguri" Mizukami
+// @license     GPL-3.0-only; https://www.gnu.org/licenses/gpl-3.0.txt
+// @description Open bsky.app links via Bridgy Fed or PDS
+// ==/UserScript==
+
+(() => {
+    // INIT
+
+    let searchInput = document.getElementsByClassName('search__input')[0];
+    if (!searchInput) {
+        function searchForSearchInput(records, observer) {
+            for (const { addedNodes } of records) {
+                for (const node of addedNodes) {
+                    searchInput = node.getElementsByClassName('search__input')[0];
+                    if (searchInput) {
+                        observer.disconnect();
+                        return;
+                    }
+                }
+            }
+        }
+        new MutationObserver(searchForSearchInput)
+            .observe(document.body, {
+                childList: true,
+                subtree: true,
+            });
+    }
+
+    const columnsArea = document.getElementsByClassName('columns-area')[0];
+    if (columnsArea) {
+        new MutationObserver(observeNewColumns)
+            .observe(columnsArea, {
+                childList: true,
+            });
+    } else {
+        function searchForColumnsArea(records, observer) {
+            for (const { addedNodes } of records) {
+                for (const node of addedNodes) {
+                    if (node.classList?.contains('columns-area')) {
+                        new MutationObserver(observeNewColumns)
+                            .observe(node, {
+                                childList: true,
+                            });
+                        observer.disconnect();
+                    }
+                }
+            }
+        }
+        new MutationObserver(searchForColumnsArea)
+            .observe(document.body, {
+                childList: true,
+                subtree: true,
+            });
+    }
+
+    const feedObserver = new MutationObserver(observeNewArticles);
+    function observeNewColumns(records, _observer) {
+        for (const { addedNodes } of records) {
+            for (const node of addedNodes) {
+                if (node.classList?.contains('column')) {
+                    const feed = node.querySelector('.item-list[role="feed"]');
+                    if (feed) {
+                        feedObserver.observe(feed, {
+                            childList: true,
+                        });
+                    } else if (node instanceof Element) {
+                        hookDescendantBskyLinks(node);
+                    }
+                }
+            }
+        }
+    }
+
+    function observeNewArticles(records, _observer) {
+        for (const { addedNodes } of records) {
+            for (const node of addedNodes) {
+                if (node instanceof Element) {
+                    hookDescendantBskyLinks(node);
+                }
+            }
+        }
+    }
+
+    const acceptAs2Headers = new Headers([['accept', 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"']]);
+    function hookDescendantBskyLinks(elt) {
+        for (const anchor of elt.querySelectorAll('a.unhandled-link:not(.status-url-link)[href^="https://bsky.app/profile/"]')) {
+            const components = bskyPostComponents(anchor.href) || [];
+            if (components) {
+                let authority = components[0];
+                const rkey = components[1];
+                let bridgeUrl = bridgeUrlFromBskyComponents(authority, rkey);
+                let verifiedBridge;
+                anchor.addEventListener('click', e => {
+                    e.preventDefault();
+                    if (verifiedBridge) {
+                        submitSearch(bridgeUrl);
+                        return;
+                    }
+                    fetch(bridgeUrl, {
+                        method: 'HEAD',
+                        headers: acceptAs2Headers,
+                        referer: '',
+                    })
+                        .then(async res => {
+                            if (res.ok) {
+                                verifiedBridge = true;
+                                return submitSearch(bridgeUrl);
+                            }
+                            if (!authority.startsWith('did:')) {
+                                authority = await resolveAtHandle(authority);
+                                bridgeUrl = bridgeUrlFromBskyComponents(authority, rkey);
+                            }
+                            open(await pdsXrpcUrlForBskyComponents(authority, rkey));
+                        });
+                });
+            }
+        }
+    }
+
+    // UTILITIES - Fedibird
+
+    function submitSearch(query) {
+        if (!searchInput) return;
+        const setValue = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+        setValue.call(searchInput, query);
+        searchInput.dispatchEvent(new Event('input', { bubbles: true }));
+        // FIXME: Doesn't work
+        searchInput.dispatchEvent(new KeyboardEvent('keyup', { key: 'Enter' }));
+    }
+
+    // UTILITIES - AT Protocol
+
+    const acceptDnsJsonHeaders = new Headers([['accept', 'application/dns-json']]);
+    async function resolveAtHandle(handle) {
+        try {
+            const res = await fetch(`https://cloudflare-dns.com/dns-query?name=_atproto.${handle}&type=TXT`, {
+                headers: acceptDnsJsonHeaders,
+                referer: '',
+            });
+            if (res.ok) {
+                const json = await res.json();
+                const expectedName = `_atproto.${handle}`;
+                for (const answer of json?.Answer ?? []) {
+                    if (answer.name === expectedName && answer.type === 16 && answer.data?.startsWith('"did=') && answer.data.endsWith('"')) {
+                        return answer.data.slice(5, -1);
+                    }
+                }
+            }
+        } catch {
+            // Fall back on well-known
+        }
+        try {
+            const res = await fetch(`https://${handle}/.well-known/atproto-did`, {
+                method: 'HEAD',
+                referer: '',
+            });
+            if (res.ok) {
+                return await res.text().trim();
+            }
+        } catch {
+            // noop
+        }
+    }
+
+    function pdsFromDidDoc(doc) {
+        for (const service of asArray(doc.service)) {
+            if (asArray(service.type).includes('AtprotoPersonalDataServer')) {
+                return service.serviceEndpoint;
+            }
+        }
+    }
+
+    const acceptDidHeaders = new Headers([['accept', 'application/did+ld+json']]);
+    async function resolveDid(did) {
+        let res;
+
+        if (did.startsWith('did:plc:')) {
+            res = await fetch(`https://plc.directory/${did}`, {
+                headers: acceptDidHeaders,
+                referer: '',
+            });
+        } else if (did.startsWith('did:web:')) {
+            res = await fetch(`https://${did.split(8)}/.well-known/did.json`, {
+                headers: acceptDidHeaders,
+                referer: '',
+            });
+        }
+
+        if (!res.ok) {
+            throw new Error(`Encountered HTTP ${res.status} status while resolving DID ${did}`);
+        }
+
+        return await res.json();
+    }
+
+    function asArray(value) {
+        if (value instanceof Array) {
+            return value;
+        } else if (value === null || value === undefined) {
+            return [];
+        } else {
+            return [value];
+        }
+    }
+
+    // UTILITIES - Bluesky
+
+    function bskyPostComponents(url) {
+        const segments = url.split('/');
+        const authority = segments[4];
+        if (authority === undefined) {
+            return;
+        }
+        const collection = segments[5];
+        if (collection === undefined) {
+            return [authority];
+        }
+        const rkey = segments[6];
+        if (collection === 'post' && rkey !== undefined) {
+            return [authority, rkey];
+        }
+    }
+
+    function bridgeUrlFromBskyComponents(authority, rkey) {
+        const at = rkey === undefined ? `at://${authority}` : `at://${authority}/app.bsky.feed.post/${rkey}`;
+        return `https://bsky.brid.gy/convert/ap/${at}`;
+    }
+
+    async function pdsXrpcUrlForBskyComponents(did, rkey) {
+        const pds = pdsFromDidDoc(await resolveDid(did));
+        return rkey === undefined
+            ? `${pds}/xrpc/com.atproto.repo.describeRepo?repo=${did}`
+            : `${pds}/xrpc/com.atproto.repo.getRecord?repo=${did}&collection=app.bsky.feed.post&rkey=${rkey}`;
+    }
+})();
